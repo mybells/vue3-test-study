7662fbb573eb185270181c25c5cbd83828f50885
mybells <435541908@qq.com>
2020-08-18 18:15:22 +0800

    update: jest test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -6,4 +6,10 @@ describe("demo1.vue", () => {
     const wrapper = shallowMount(demo1, {});
     expect(wrapper.text()).toMatch("msg is 1");
   });
+  test("1+1==2?", () => {
+    expect(1 + 1).toBe(2);
+  });
+  test("this will be the only test that runs", () => {
+    expect(true).toBe(true);
+  });
 });
