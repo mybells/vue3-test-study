b1b30348c4f5ea173549c07aa39522f0d4937375
mybells <435541908@qq.com>
2020-08-19 18:20:14 +0800

    update: jest api

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -2,6 +2,11 @@ import { shallowMount } from "@vue/test-utils";
 import demo1 from "@/components/demo1.vue";
 
 describe("demo1.vue", () => {
+  beforeAll(() => console.log(1));
+  beforeEach(() => console.log(2));
+  afterEach(() => console.log(4));
+  afterAll(() => console.log(3));
+  //执行顺序：12424243
   it("renders props.msg when passed", () => {
     const wrapper = shallowMount(demo1, {});
     expect(wrapper.text()).toMatch("msg is 1");
